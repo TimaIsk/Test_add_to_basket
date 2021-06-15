9a76dc3979dbf2b3f0ad63a347659300e80b34bd
TimaIsk <84448372+TimaIsk@users.noreply.github.com>
2021-06-15 10:48:45 +0500

    Check basket button

---
+++ test_items.py
@@ -0,0 +1,11 @@
+import time
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+
+def test_add_basket_button(browser):
+    browser.get(link)
+    time.sleep(5)
+    button = browser.find_element_by_xpath('//button[@class="btn btn-lg btn-primary btn-add-to-basket"]').is_enabled()
+
+    assert button, " Mission: Impossible"
