59e1a66b7b1d50a593e6f0f80fe14fc5ed1092fe
TimaIsk <84448372+TimaIsk@users.noreply.github.com>
2021-06-15 14:49:23 +0500

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -6,7 +6,7 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 def test_add_basket_button(browser):
     browser.get(link)
     time.sleep(30)
-    button = browser.find_element_by_xpath('//button[@class="btn btn-lg btn-primary btn-add-to-basket"]')
+    button = len(browser.find_elements_by_css_selector('.btn-add-to-basket'))
 
-    assert len(button) > 0, "Mission: Impossible"
+    assert button > 0, "Mission: Impossible"
     
