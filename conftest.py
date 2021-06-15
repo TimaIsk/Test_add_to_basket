1cd8689293674b5d08f572ee59ed42117340479a
TimaIsk <84448372+TimaIsk@users.noreply.github.com>
2021-06-15 11:30:25 +0500

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -3,17 +3,29 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 def pytest_addoption(parser):
-    parser.addoption('--language', action='store', default=None, help="Choose language: ru, en, ....")
+    parser.addoption('--browser_name', action='store', default='chrome', help="Choose browser: chrome or firefox")
+    parser.addoption('--language', action='store', default='ru', help="Choose language: ru or en")
 
 @pytest.fixture(scope="function")
 def browser(request):
+    browser_name = request.config.getoption("browser_name")
     user_language = request.config.getoption('language')
-    options = Options()
-    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
-    options.add_experimental_option('excludeSwitches', ['enable-logging'])
-    browser = webdriver.Chrome(options=options)
-    browser.maximize_window()
+    browser = None
+    if browser_name == "chrome":
+        print("\nstart chrome browser for test..")
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+        options.add_experimental_option('excludeSwitches', ['enable-logging'])
+        browser = webdriver.Chrome(options=options)
+        browser.maximize_window()
+    elif browser_name == "firefox":
+        print("\nstart firefox browser for test..")
+        fp = webdriver.FirefoxProfile()
+        fp.set_preference("intl.accept_languages", user_language)
+        browser = webdriver.Firefox(firefox_profile=fp)
+        browser.maximize_window()
+    else:
+       raise pytest.UsageError("--browser_name should be chrome or firefox")
     yield browser
     print("\nquit browser..")
     browser.quit()
-
